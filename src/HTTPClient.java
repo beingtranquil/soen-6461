883c69ded1c5fdfdcf832328d70597ccea96bfc3
being_tranquil <panchaldhruv0149@gmail.com>
2023-10-03 15:49:04 -0400

    c1

--- src/HTTPClient.java
+++ src/HTTPClient.java
@@ -1,2 +1,67 @@
-package PACKAGE_NAME;public class HTTPClient {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HTTPClient {
+    public static String sendGETRequest(String url) throws IOException {
+        HttpURLConnection connection = null;
+        try {
+            URL getUrl = new URL(url);
+            connection = (HttpURLConnection) getUrl.openConnection();
+
+            // Set HTTP method to GET
+            connection.setRequestMethod("GET");
+
+            // Read response
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            reader.close();
+
+            return response.toString();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+    }
+
+    public static String sendPOSTRequest(String url, String postData) throws IOException {
+        HttpURLConnection connection = null;
+        try {
+            URL postUrl = new URL(url);
+            connection = (HttpURLConnection) postUrl.openConnection();
+
+            // Set HTTP method to POST
+            connection.setRequestMethod("POST");
+            connection.setDoOutput(true);
+
+            // Write POST data
+            OutputStream os = connection.getOutputStream();
+            os.write(postData.getBytes());
+            os.flush();
+            os.close();
+
+            // Read response
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            reader.close();
+
+            return response.toString();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length < 1) {
+            System.out.println("Usage: java Main [URL] [POST_DATA (optional)]");
+            return;
+        }
+
+        String url = args[0];
+        String postData = args.length > 1 ? args[1] : null;
+
+        try {
+            if (postData != null) {
+                String response = HTTPClient.sendPOSTRequest(url, postData);
+                System.out.println("POST Response:\n" + response);
+            } else {
+                String response = HTTPClient.sendGETRequest(url);
+                System.out.println("GET Response:\n" + response);
+            }
+        } catch (IOException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
     }
 }
